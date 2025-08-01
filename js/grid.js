d59ec6828d60c966e1affb0e6cfde89840e393dd
k1ll1-9 <k1ll1-9@yandex.ru>
2025-08-01 07:42:49 +0300

    hotfix

--- js/grid.js
+++ js/grid.js
@@ -2,7 +2,9 @@
  * Меняем URL запроса на бэк на корректный
  */
 BX.addCustomEvent('Grid::beforeRequest', (gridObj, configObj) => {
-        if (configObj.gridId === 'CRM_DEAL_CUSTOM_GRID' && configObj.url !== '/local/modules/test.crmgrid/lib/ajax/grid.php')
-            configObj.url = '/local/modules/test.crmgrid/lib/ajax/grid.php';
+        if (configObj.gridId === 'CRM_DEAL_CUSTOM_GRID' && configObj.url !== '/local/modules/test.crmgrid/lib/ajax/grid.php') {
+            const grid = BX.Main.gridManager.getById('CRM_DEAL_CUSTOM_GRID')
+            grid.instance.baseUrl = '/local/modules/test.crmgrid/lib/ajax/grid.php'
+        }
     }
 );
